c4c35f8c1a7082d0536e3a2318ac0b74795336b6
huangls <huangls@fxft.net>
2019-10-24 19:45:54 +0800

    license接口

---
+++ src/main/java/net/fxft/webgateway/license/LicenseController.java
@@ -0,0 +1,28 @@
+package net.fxft.webgateway.license;
+
+import net.fxft.common.log.RestExecuter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/license")
+public class LicenseController {
+
+    private static final Logger log = LoggerFactory.getLogger(LicenseController.class);
+    
+    @Autowired
+    private LicenseUtil licenseUtil;
+
+    @RequestMapping("/getVehicleLimitCount")
+    public String getVehicleLimitCount() {
+        return RestExecuter.build(log, "getVehicleLimitCount")
+                .run(alog -> {
+                    License bean = licenseUtil.loadLicense();
+                    return bean.getDeviceAmount();
+                });
+    }
+
+}
