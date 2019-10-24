1f53e39d24e1858f4dc4112a76dd474ee7bd49ac
hzz <605198347@qq.com>
2019-10-24 15:03:57 +0800

    对外接口放到最上面去就可以了

--- src/main/java/net/fxft/webgateway/route/GatewayRoutes.java
+++ src/main/java/net/fxft/webgateway/route/GatewayRoutes.java
@@ -38,6 +38,7 @@ public class GatewayRoutes {
                         "/vehicleActionAPI/**",
                         "/appimg/getAppQRCodeImg.action",
                         "/AppQRCodePicture/**",
+                        "/globalplatformconfig/**",
                         "/platformconfig/getIpDomainPlatfromConfig.action",
                         "/platformconfig/getGlobalPlatfromConfig.action"))
                         .filters(f -> f.filter(removeHeaderFilter))
