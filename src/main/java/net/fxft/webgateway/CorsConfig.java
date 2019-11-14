c7915a8bb4e0699235fe275bcd0dc88b758aefb6
zhanghl <848168348@qq.com>
2019-11-14 13:50:44 +0800

    修改跨域数据库读取

--- src/main/java/net/fxft/webgateway/CorsConfig.java
+++ src/main/java/net/fxft/webgateway/CorsConfig.java
@@ -1,9 +1,12 @@
 package net.fxft.webgateway;
 
+import net.fxft.common.jdbc.JdbcUtil;
+import net.fxft.common.jdbc.JdbcUtil.Operator;
 import net.fxft.common.util.BasicUtil;
 import org.apache.mina.util.ConcurrentHashSet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,15 +16,20 @@ import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.util.pattern.PathPatternParser;
 
+import javax.annotation.PostConstruct;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Configuration
 public class CorsConfig {
-    
+
     private static final Logger log = LoggerFactory.getLogger(CorsConfig.class);
 
     private static CorsConfiguration corsConfig;
     private static Set<String> orginSet = new ConcurrentHashSet<>();
+    @Autowired
+    private JdbcUtil jdbc;
 
     @Bean
     public CorsWebFilter corsFilter(@Value("${cors.orgins}") String origins) {
@@ -29,11 +37,12 @@ public class CorsConfig {
         corsConfig.addAllowedMethod("*");
         corsConfig.addAllowedHeader("*");
         corsConfig.setAllowCredentials(true);
+        updateOrgin();
         log.info("cors.AllowedOrigin=" + origins);
         for (String origin : origins.split(",")) {
             origin = origin.trim();
-            if(BasicUtil.isNotEmpty(origin)) {
-                corsConfig.addAllowedOrigin(origin);
+            if (BasicUtil.isNotEmpty(origin)) {
+                addOrigin(origin);
             }
         }
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
@@ -43,11 +52,36 @@ public class CorsConfig {
 
     public static void addOrigin(String origin) {
         corsConfig.addAllowedOrigin(origin);
+        orginSet.add(origin);
     }
 
-    @Scheduled(fixedDelay = 60000)
+    @Scheduled(fixedDelay = 30000)
     public void updateOrgin() {
+        Set<String> ipDomain = getipDomain();
+        if (ipDomain.size() <= 0) {
+            return;
+        }
+        ipDomain.stream().filter(info -> {
+            if (orginSet.contains(info)) {
+            return false;
+            }
+            return true;
+        }).forEach(info -> {
+            addOrigin(info);
 
+        });
+      }
+
+    public Set<String> getipDomain() {
+        String sql = "select ip_domain from platform_config where #{aaa}";
+        List<String> query = jdbc.sql(sql)
+                .whereName("aaa")
+                .and("ip_domain", Operator.RightLike, "http")
+                .endWhere()
+                .query(String.class);
+        Set<String> ipDomain = new HashSet<>(query);
+        return ipDomain;
     }
+
 
 }
--- src/test/java/net/fxft/webgateway/WebGatewayApplicationTests.java
+++ src/test/java/net/fxft/webgateway/WebGatewayApplicationTests.java
@@ -1,6 +1,9 @@
 package net.fxft.webgateway;
 
 import net.fxft.cloud.http.HttpUtil;
+import net.fxft.common.jdbc.JdbcUtil;
+import net.fxft.common.jdbc.JdbcUtil.Operator;
+import org.apache.mina.util.ConcurrentHashSet;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,11 +15,13 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
 
 import java.lang.management.ManagementFactory;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class WebGatewayApplicationTests {
-
 
 
     @Autowired
@@ -24,11 +29,13 @@ public class WebGatewayApplicationTests {
     @Autowired
     private HttpUtil httpUtil;
 
+    @Autowired
+    private JdbcUtil jdbc;
 
     @Test
     public void contextLoads() {
         String str = rt.getForObject("http://security/api/userVehiclePermission?userId=65", String.class);
-        System.out.println("=========="+str );
+        System.out.println("==========" + str);
     }
 
     @Test
@@ -43,9 +50,37 @@ public class WebGatewayApplicationTests {
 //        System.out.println(str);
 
         String str1 = httpUtil.get().url(url).executeString();
-        System.out.println("=============="+str1);
+        System.out.println("==============" + str1);
 //        map.remove("cId");
 //        String responseEntity= restTemplate.getForObject(url,String.class,map);
     }
+
+    private static Set<String> orginSet = new ConcurrentHashSet<>();
+
+    @Test
+    public void test3() {
+        Set<String> ipDomain = getipDomain();
+        Set<String> set = new HashSet<>(ipDomain);
+        if (orginSet.size() > 0) {
+            set.retainAll(orginSet);
+        }
+        set.stream().forEach(info -> {
+            System.out.println(info);
+        });
+        orginSet = ipDomain;
+    }
+
+
+    public Set<String> getipDomain() {
+        String sql = "select ip_domain from platform_config where #{aaa}";
+        List<String> query = jdbc.sql(sql)
+                .whereName("aaa")
+                .and("ip_domain", Operator.RightLike, "http")
+                .endWhere()
+                .query(String.class);
+       return new HashSet<>(query);
+
+    }
+
 
 }
