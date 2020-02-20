227ae190cc754a6c8d94663fea48901ef894b555
zhangwj <https://git.fxft>
2020-02-20 13:40:53 +0800

    ip问题修复

--- src/main/java/net/fxft/webgateway/util/IpUtil.java
+++ src/main/java/net/fxft/webgateway/util/IpUtil.java
@@ -7,6 +7,7 @@ import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.context.request.ServletWebRequest;
 
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
 
 /**
@@ -24,36 +25,26 @@ public class IpUtil {
         String ip = headers.getFirst("X-Forwarded-For");
         if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
             ip = headers.getFirst("X-Real-IP");
-            log.debug("Ip来源X-Real-IP" + ip);
         }
         if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
             ip = headers.getFirst("Proxy-Client-IP");
-            log.debug("Ip来源Proxy-Client-IP" + ip);
 
         }
         if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
             ip = headers.getFirst("WL-Proxy-Client-IP");
-            log.debug("Ip来源WL-Proxy-Client-IP" + ip);
 
         }
         if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
             ip = headers.getFirst("HTTP_CLIENT_IP");
-            log.debug("Ip来源HTTP_CLIENT_IP" + ip);
 
         }
         if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
             ip = headers.getFirst("HTTP_X_FORWARDED_FOR");
-            log.debug("Ip来源HTTP_X_FORWARDED_FOR" + ip );
 
         }
         if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-            InetAddress address = request.getRemoteAddress().getAddress();
-            ip = address.getHostAddress();
-            byte[] address1 = address.getAddress();
-            log.debug("本地Ip：" + ip );
-            log.debug("本地Ip：" + address.getCanonicalHostName() );
-            log.debug("本地Ip：" + address.getHostName() );
-            log.debug("本地Ip：" + ByteUtil.byteToHexStr(address1) );
+            InetSocketAddress remoteAddress = request.getRemoteAddress();
+            ip = remoteAddress.getAddress().getHostAddress();
             if("127.0.0.1".equals(ip)||"0:0:0:0:0:0:0:1".equals(ip)){
                 //根据网卡取本机配置的IP
                 InetAddress inet=null;
